7da3734a99a9bd3fe50a86d93aa6471c0cd323f8
Mattis Kleine <58635065+mattiskleine@users.noreply.github.com>
2021-12-05 11:19:53 +0100

    updated structure

---
+++ objects/adam/project_adam_script.js
@@ -0,0 +1,22 @@
+window.onload = function() {
+
+}
+
+var adam = 1;
+function toggleAdam() {
+  if (adam == 1) {
+    adam = 2;
+    document.getElementById('toggle_container').style.backgroundColor = '#519E83';
+    document.getElementById('toggle_button').style.transform = 'translateX(13.4vw)';
+    document.getElementById('toggle_button').innerHTML = 'stage 2';
+    document.getElementById('adam1').style.transform = 'translateX(-180%)';
+    document.getElementById('adam2').style.transform = 'translateX(-50%)';
+  } else {
+    adam = 1;
+    document.getElementById('toggle_container').style.backgroundColor = '#5EBD9C';
+    document.getElementById('toggle_button').style.transform = 'translateX(0.4vw)';
+    document.getElementById('toggle_button').innerHTML = 'stage 1';
+    document.getElementById('adam1').style.transform = 'translateX(-50%)';
+    document.getElementById('adam2').style.transform = 'translateX(80%)';
+  }
+}
