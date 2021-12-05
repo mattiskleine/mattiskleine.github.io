103d59ae31fd0c0e53551bbae2ede09925a5bad3
Mattis Kleine <58635065+mattiskleine@users.noreply.github.com>
2021-12-05 15:17:06 +0100

    update adam

--- objects/adam/project_adam_script.js
+++ objects/adam/project_adam_script.js
@@ -9,14 +9,14 @@ function toggleAdam() {
     document.getElementById('toggle_container').style.backgroundColor = '#519E83';
     document.getElementById('toggle_button').style.transform = 'translateX(13.4vw)';
     document.getElementById('toggle_button').innerHTML = 'stage 2';
-    document.getElementById('adam1').style.transform = 'translateX(-180%)';
     document.getElementById('adam2').style.transform = 'translateX(-50%)';
+    document.getElementById('adam1').style.transform = 'translateX(-150%)';
   } else {
     adam = 1;
     document.getElementById('toggle_container').style.backgroundColor = '#5EBD9C';
     document.getElementById('toggle_button').style.transform = 'translateX(0.4vw)';
     document.getElementById('toggle_button').innerHTML = 'stage 1';
     document.getElementById('adam1').style.transform = 'translateX(-50%)';
-    document.getElementById('adam2').style.transform = 'translateX(80%)';
+    document.getElementById('adam2').style.transform = 'translateX(50%)';
   }
 }
