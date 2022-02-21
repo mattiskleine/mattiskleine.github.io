bb85096eb10f2c6bfccd93a6f32ace9c8d09cd52
Mattis Kleine <58635065+mattiskleine@users.noreply.github.com>
2022-02-21 22:41:15 +0100

    emissions_update

--- objects/emissions/project_emissions_script.js
+++ objects/emissions/project_emissions_script.js
@@ -1,21 +1,55 @@
-function getInput(x, y) {
-  if(inputActive[y] == 0) {
-    document.getElementById("text_input_"+x).value = "";
+var countriesActive = 0;
+function expandCountries() {
+  if(countriesActive == 0) {
+    countriesActive = 1;
+    document.getElementById("input_button_countries").style.width = "100%";
+    document.getElementById("input_button_countries").style.height = "760%";
+    document.getElementById("countries_placeholder").style.display = "none";
+    var item = document.getElementsByClassName("item_countries");
+    for(var i = 0; i < item.length; i++) {
+      item[i].style.display = "block";
+    }
   }
 }
 
-var inputActive = [0, 0, 0, 0, 0];
-var inputTop = ["15%", "25%", "35%", "45%", "55%"];
+function selectCountry(x) {
+  document.getElementById("input_button_countries").style.width = "7vw";
+  document.getElementById("input_button_countries").style.height = "150%";
+  document.getElementById("countries_placeholder").style.display = "block";
+  document.getElementById("countries_placeholder").innerHTML = x;
+  var item = document.getElementsByClassName("item_countries");
+  for(var i = 0; i < item.length; i++) {
+    item[i].style.display = "none";
+  }
+  setTimeout(() => {
+    countriesActive = 0;
+  }, 100);
+}
 
-function changeInput(x) {
-  if(inputActive[x] == 0) {
-    inputActive[x] = 1;
-    var y = x+1;
-    document.getElementById("option_"+x).style.left = "10%";
-    document.getElementById("option_"+x).style.top = inputTop[x];
-    document.getElementById("option_"+x).style.transform = "translateX(0) scale(1)";
-    if(x < 4) {
-      document.getElementById("option_"+y).style.transform = "translateX(-50%) scale(1)";
+var kilometersActive = 0;
+function expandKilometers() {
+  if(kilometersActive == 0) {
+    kilometersActive = 1;
+    document.getElementById("input_button_kilometers").style.width = "100%";
+    document.getElementById("input_button_kilometers").style.height = "950%";
+    document.getElementById("kilometers_placeholder").style.display = "none";
+    var item = document.getElementsByClassName("item_kilometers");
+    for(var i = 0; i < item.length; i++) {
+      item[i].style.display = "block";
     }
   }
+}
+
+function selectKilometers(x) {
+  document.getElementById("input_button_kilometers").style.width = "8vw";
+  document.getElementById("input_button_kilometers").style.height = "150%";
+  document.getElementById("kilometers_placeholder").style.display = "block";
+  document.getElementById("kilometers_placeholder").innerHTML = x;
+  var item = document.getElementsByClassName("item_kilometers");
+  for(var i = 0; i < item.length; i++) {
+    item[i].style.display = "none";
+  }
+  setTimeout(() => {
+    kilometersActive = 0;
+  }, 100);
 }
